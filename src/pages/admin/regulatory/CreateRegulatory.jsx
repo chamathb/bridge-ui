3672c469a757b9b6c30ce47be241966d22ef7a5d
Hasindu Nishakara <nishakara.ac@gmail.com>
2019-05-21 13:08:44 +0530

    handled the routing error

--- src/pages/admin/regulatory/CreateRegulatory.jsx
+++ src/pages/admin/regulatory/CreateRegulatory.jsx
@@ -1,71 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Formik } from "formik";
 import * as Yup from "yup";
 import AppHeader from '../../../component/AppHeader/AppHeader';
 import MenuDrawer from "../../../component/MenuDrawer/MenuDrawer";
-//import CreateClientForm from './forms/CreateClientForm';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import { getClient, addContact } from './state/Actions';
 import TextField from '@material-ui/core/TextField';
 import Divider from '@material-ui/core/Divider';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import CreateRegulatoryForm from "./forms/CreateRegulatoryForm";
+import CreateRegulatoryForm from './forms/CreateRegulatoryForm';
 
 const styles = theme => ({
-  root : {
-    display : 'flex',
-      
+  root: {
+    display: 'flex',
+
   },
-  content : {
-    flexGrow : 1,
-    padding : theme.spacing.unit * 3 + 1,
-    marginTop : theme.spacing.unit,
-    maxWidth : "1024px",
-    position : "relative",
-    paddingBottom : '100px',
-    display : 'flex',
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3 + 1,
+    marginTop: theme.spacing.unit,
+    maxWidth: "1024px",
+    position: "relative",
+    paddingBottom: '100px',
+    display: 'flex',
 
   },
   body: {
-    paddingTop : theme.spacing.unit * 6,
+    paddingTop: theme.spacing.unit * 6,
     display: 'flex',
 
   },
 
-  saveButton : {
-    marginLeft : '10px',
-  },
-
-  buttonContainer:{
-    alignItems:'right',
-    justifyContent:'flex-end',
-    display: 'flex',
-
-  },
-
-  saveButton:{
-    marginLeft : '10px',
+  saveButton: {
+    marginLeft: '10px',
   },
 
   buttonContainer: {
     alignItems: 'right',
-    justifyContent : 'flex',
-    display : 'flex',
+    justifyContent: 'flex-end',
+    display: 'flex',
 
   },
 
-  cancelButton : {
-    mariginLeft : '10px',
+  saveButton: {
+    marginLeft: '10px',
+  },
+
+  buttonContainer: {
+    alignItems: 'right',
+    justifyContent: 'flex',
+    display: 'flex',
 
   },
-  
+
+  cancelButton: {
+    mariginLeft: '10px',
+
+  },
+
   extendedIcon: {
-    marginRight : theme.spacing.unit,
+    marginRight: theme.spacing.unit,
   },
 
   addAnotherClient: {
@@ -78,7 +78,7 @@ const styles = theme => ({
 
 const valiadtionSchema = Yup.object({
 
-  name:Yup.string("Authority Institution Name")
+  name: Yup.string("Authority Institution Name")
     .required("Authority Institution Name cannot be empty"),
   testName: Yup.string("Refernce Test Name")
     .required("Reference Test Name cannot be empty"),
@@ -86,79 +86,89 @@ const valiadtionSchema = Yup.object({
     .required("This field cannot be empty"),
   requirement: Yup.string("Sample Requirement")
     .required("Sample Requiremnt cannot be empty"),
-  averageRelease:Yup.string("Average Release Time")
+  averageRelease: Yup.string("Average Release Time")
     .required("Average release time cannot be empty"),
-  stage:Yup.string("Approval Obtaining Stage")
+  stage: Yup.string("Approval Obtaining Stage")
     .required("Approval obtaing stage cannot be empty"),
-  attachements:Yup.array()
+
+  attachements: Yup.array()
     .of(
       Yup.object().shape({
         docName: Yup.string("Document Name")
           .required("Document name cannot be empty"),
         description: Yup.string("Description")
           .required("Description cannot be empty"),
-        mandatory : Yup.string("Mandatory")
+        mandatory: Yup.string("Mandatory")
           .required("Mandatory field cannot be empty"),
 
       })
     )
+  
 });
 
-class CreateRegulatory extends React.Component{
-  constructor(props)
-  {
+class CreateRegulatory extends React.Component {
+  constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
+      regulatory:{ 
+        name :"",
+        testName: "",
+        release: "",
+        Timerequirement:"",
+        averageRelease:"",
+        stage:"",
+
+          attachment : [{
+            docName: "",
+            description: "",
+            mandatory : "",
+          }]
+
+      }
+
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
   }
 
-  render(){
-    const{classes}  = this.props;
-    const initialValue = {
-      name: '',
-      testName: '',
-      releaseTime: '',
-      requirement: '',
-      averageRelease: '',
-      stage: '',
-      attachments: [{
-        docName: '',
-        description: '',
-        mandatory: '',
-      }],
-    };
-    return(
-      <div className= {classes.root}>
-      <CssBaseline/>
-      <AppHeader/>
+  
+  render() {
 
-      <div className = {classes.body}>
-      <MenuDrawer />
+    const {classes} = this.props;
+    //const { classes, regulatory } = this.props;
+    //const initialValue = this.state.client;
 
-      <div className = {classes.content}>
-      <br/>
-      <Grid container spacing = {24}>
-        <Grid item xs = {12} md = {12}>
-        <Typography variant = "h5">
-        Attachements
+    
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppHeader />
+
+        <div className={classes.body}>
+          <MenuDrawer />
+
+          <div className={classes.content}>
+            <br />
+            <Grid container spacing={24}>
+              <Grid item xs={12} md={12}>
+                <Typography variant="h5">
+                  Attachements
         </Typography>
-        </Grid>
-        <Grid>
-          <Formik 
-            render = {props =>
-            <CreateRegulatoryForm {...props} />}
-            initialValues= {initialValue}
-            validationSchema = {valiadtionSchema}/>
-          </Grid>
-          </Grid>  
+              </Grid>
+              <Grid>
+                <Formik
+                  render={props =>
+                    <CreateRegulatoryForm {...props} />}
+                  //initialValues={initialValue} 
+                  validationSchema={valiadtionSchema} />
+              </Grid>
+            </Grid>
           </div>
         </div>
       </div>
-          
+
 
     );
   }
@@ -169,7 +179,7 @@ CreateRegulatory.propTypes = {
 
 };
 
-const mapStateToProps = ({Regulatory}) => {
+const mapStateToProps = ({regulatory }) => {
 
 };
 
@@ -177,4 +187,4 @@ const mapActionToProps = {
 
 };
 
-export default connect (mapStateToProps, mapActionToProps) (withStyles(styles)(CreateRegulatory));
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(CreateRegulatory));
--- src/pages/admin/regulatory/forms/CreateRegulatoryForm.js
+++ src/pages/admin/regulatory/forms/CreateRegulatoryForm.js
@@ -14,7 +14,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 
 const RegulatoryAttachPaper = withStyles({
   root: {
-    backgroundColor:'transparent',
+    backgroundColor: 'transparent',
     borderColor: 'red',
     border: '1',
   },
@@ -23,7 +23,7 @@ const RegulatoryAttachPaper = withStyles({
 const CreateRegulatoryForm = props => {
   const {
     values: {
-      name, testName, releaseTime, requirement, averageRelease, stage, attachments
+      name, testName, releaseTime, requirement, averageRelease, stage,
     },
     errors,
     touched,
@@ -38,189 +38,191 @@ const CreateRegulatoryForm = props => {
     setFieldTouched(name, true, false);
   };
 
-  const attachment = { docName : "", description : "", mandatory : ""};
-  return(
-    <form onSubmit = {handleSubmit}>
-    <Grid  container spacing = {24}>
-        <Grid item xs = {6} md = {6}>
-        <Field id = "name"
-          name = "name"
-          helperText = {touched.name ? errors.name : ""}
-          error = { touched.name && Boolean(errors.name)}
-          value = {name}
-          onChange = {change.bind(null, "name")}
-          label = "Authority / Institute Name"
-          fullWidth
-          component = {TextField}
-          variant = "outlined" />
-          </Grid>
+  //const regulatoryAttch = { name: "", testName: "", releaseTime: "", requirement: "", averageRelease: "", stage: "" };
+  const attachment = { docName: "", description: "", mandatory: "" };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={24}>
+        <Grid item xs={6} md={6}>
+          <Field id="name"
+            name="name"
+            helperText={touched.name ? errors.name : ""}
+            error={touched.name && Boolean(errors.name)}
+            value={name}
+            onChange={change.bind(null, "name")}
+            label="Authority / Institute Name"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
 
-          <Grid item xs = {6} md= {6}>
-            <Field id = "testName"
-                  name =  "testName"
-                  value = {testName}
-                  helperText = {touched.testName ? errors.testName : ""}
-                  error = {touched.testName && Boolean(errors.testName)}
-                  onChange = {change.bind(null, "testName")}
-                  label = "Reference Test Name"
-                  fullWidth
-                  component= {TextField}
-                  variant = "outlined" />
-          </Grid>
+        <Grid item xs={6} md={6}>
+          <Field id="testName"
+            name="testName"
+            value={testName}
+            helperText={touched.testName ? errors.testName : ""}
+            error={touched.testName && Boolean(errors.testName)}
+            onChange={change.bind(null, "testName")}
+            label="Reference Test Name"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
 
-          <Grid item xs = {6} md= {6}>
-            <Field id = "releaseTime"
-                  name =  "releaseTime"
-                  value = {releaseTime}
-                  helperText = {touched.releaseTime ? errors.releaseTime : ""}
-                  error = {touched.releaseTime && Boolean(errors.releaseTime)}
-                  onChange = {change.bind(null, "releaseTime")}
-                  label = "Release Time In Date"
-                  fullWidth
-                  component= {TextField}
-                  variant = "outlined" />
-          </Grid>
+        <Grid item xs={6} md={6}>
+          <Field id="releaseTime"
+            name="releaseTime"
+            value={releaseTime}
+            helperText={touched.releaseTime ? errors.releaseTime : ""}
+            error={touched.releaseTime && Boolean(errors.releaseTime)}
+            onChange={change.bind(null, "releaseTime")}
+            label="Release Time In Date"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
 
-          <Grid item xs = {6} md= {6}>
-            <Field id = "requirement"
-                  name =  "requirement"
-                  value = {requirement}
-                  helperText = {touched.requirement ? errors.requirement : ""}
-                  error = {touched.requirement && Boolean(errors.requirement)}
-                  onChange = {change.bind(null, "requirement")}
-                  label = "Sample Requirement"
-                  fullWidth
-                  component= {TextField}
-                  variant = "outlined" />
-          </Grid>
+        <Grid item xs={6} md={6}>
+          <Field id="requirement"
+            name="requirement"
+            value={requirement}
+            helperText={touched.requirement ? errors.requirement : ""}
+            error={touched.requirement && Boolean(errors.requirement)}
+            onChange={change.bind(null, "requirement")}
+            label="Sample Requirement"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
 
-          <Grid item xs = {6} md= {6}>
-            <Field id = "averageRelease"
-                  name =  "averageRelease"
-                  value = {averageRelease}
-                  helperText = {touched.averageRelease ? errors.averageRelease : ""}
-                  error = {touched.averageRelease && Boolean(errors.averageRelease)}
-                  onChange = {change.bind(null, "averageRelease")}
-                  label = "Average Release Time"
-                  fullWidth
-                  component= {TextField}
-                  variant = "outlined" />
-          </Grid>
+        <Grid item xs={6} md={6}>
+          <Field id="averageRelease"
+            name="averageRelease"
+            value={averageRelease}
+            helperText={touched.averageRelease ? errors.averageRelease : ""}
+            error={touched.averageRelease && Boolean(errors.averageRelease)}
+            onChange={change.bind(null, "averageRelease")}
+            label="Average Release Time"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
 
-          <Grid item xs = {6} md= {6}>
-            <Field id = "stage"
-                  name =  "stage"
-                  value = {stage}
-                  helperText = {touched.stage ? errors.stage : ""}
-                  error = {touched.stage && Boolean(errors.stage)}
-                  onChange = {change.bind(null, "stage")}
-                  label = "Approval Obtaning Stage"
-                  fullWidth
-                  component= {TextField}
-                  variant = "outlined" />
-          </Grid>
-     </Grid>
-              <br />
-              <br />
+        <Grid item xs={6} md={6}>
+          <Field id="stage"
+            name="stage"
+            value={stage}
+            helperText={touched.stage ? errors.stage : ""}
+            error={touched.stage && Boolean(errors.stage)}
+            onChange={change.bind(null, "stage")}
+            label="Approval Obtaning Stage"
+            fullWidth
+            component={TextField}
+            variant="outlined" />
+        </Grid>
+      </Grid>
+      <br />
+      <br />
 
-              <Divider variant = "middle" />
-              <br />
-              <FieldArray
-                name = "attachments"
-                render= {arrayHelpers => (
-                  <div>
-                    <Grid container direction = "row" justify = "space-between" alignItems = "baseline" >
-                      <Grid item>
-                        <Typography variant = "sub-title">
-                        Attachements
+      <Divider variant="middle" />
+      <br />
+      <FieldArray
+        name="attachments"
+        render={arrayHelpers => (
+          <div>
+            <Grid container direction="row" justify="space-between" alignItems="baseline" >
+              <Grid item>
+                <Typography variant="sub-title">
+                  Attachments
                         </Typography>
-                      </Grid>
-                      <Grid item >
-                        <Button color = "primary"
-                            onClick = {() => {arrayHelpers.push(attachment)}}>
-                              {attachments && attachments.length >0 ? "Add Another Contact" : "Add Contact"}
-
-                        </Button>
-                      </Grid>
-                    </Grid>
-                    <br />
+              </Grid>
+              <Grid item >
+                <Button color="primary"
+                  onClick={() => { arrayHelpers.push(attachment) }}>
+                  {attachment && attachment.length > 0 ? "Add Another Attachment" : "Add Attachment"}
 
 
-                    {
-                      attachments.map ((attachment, index) =>
-                      (
-                        <RegulatoryAttachPaper elevation= {0}>
-                          {index > 0 ? <React.Fragment>
-                            <br/>
-                            <Divider variant = "middle" />
-                            <br/>
-                            </React.Fragment> : ""}
-                            
-                            <Grid container direction = "row" spacing = {0} justify = "space-between" alignItems= "center">
-                              <Grid item xs={11}>
-                               <Grid container direction="row" spacing={8} justify="space-between">
-                   
-                                <Grid item xs ={4}>
-                                  <Field name ={`attachment.${index}.name`}
-                                      label  = "Document Name"
-                                      fullWidth
-                                      component = {TextField}
-                                      variant = "outlined"/>
-                               </Grid> 
-                               <Grid item xs = {3}>
-                                 <Field name = {`attachment.${index}.description`}
-                                      label = "Description"
-                                      fullWidth
-                                      component =  {TextField}
-                                      variant = "outlined"/>
+                </Button>
+              </Grid>
+            </Grid>
+            <br />
 
-                            </Grid>
 
-                            <Grid item xs= {3}>
-                              <Field name ={`attachment.${index}.mandatory`}
-                                label = "Mandatory"
-                                fullWidth
-                                component = {TextField}
-                                variant = "outlined"/>
-                            </Grid>
-                            
-                          <Grid item>
-                              <IconButton aria-label = "Delete"
-                                onClick = {() => arrayHelpers.remove(index)}>
-                                  <DeleteIcon/>
-                              </IconButton>
+            {
+              attachment.map((attachment, index) =>
+                (
+                  <RegulatoryAttachPaper elevation={0}>
+                    {index > 0 ? <React.Fragment>
+                      <br />
+                      <Divider variant="middle" />
+                      <br />
+                    </React.Fragment> : ""}
+
+                    <Grid container direction="row" spacing={0} justify="space-between" alignItems="center">
+                      <Grid item xs={11}>
+                        <Grid container direction="row" spacing={8} justify="space-between">
+
+                          <Grid item xs={4}>
+                            <Field name={`attachment.${index}.name`}
+                              label="Document Name"
+                              fullWidth
+                              component={TextField}
+                              variant="outlined" />
                           </Grid>
+                          <Grid item xs={3}>
+                            <Field name={`attachment.${index}.description`}
+                              label="Description"
+                              fullWidth
+                              component={TextField}
+                              variant="outlined" />
+
+                          </Grid>
+
+                          <Grid item xs={3}>
+                            <Field name={`attachment.${index}.mandatory`}
+                              label="Mandatory"
+                              fullWidth
+                              component={TextField}
+                              variant="outlined" />
+                          </Grid>
+
+                          <Grid item>
+                            <IconButton aria-label="Delete"
+                              onClick={() => arrayHelpers.remove(index)}>
+                              <DeleteIcon />
+                            </IconButton>
+                          </Grid>
+                        </Grid>
                       </Grid>
                     </Grid>
-                  </Grid>
 
-                      </RegulatoryAttachPaper> ))
-                    }
-                    <br/>
-                    <Divider variant = "fullWidth"/>
-                    <br/>
+                  </RegulatoryAttachPaper>))
+            }
+            <br />
+            <Divider variant="fullWidth" />
+            <br />
 
-                    <Grid container
-                          spacing ={8}
-                          direction = "row-reverse"
-                          justify = "flex-start"
-                          alignItems = "flex-start">
-                    <Grid item>
-                      <Button type = "submit" variant = "contained" color= "primary" disabled = {!isValid}>
-                      Save
-                      </Button>  
-                    </Grid>
+            <Grid container
+              spacing={8}
+              direction="row-reverse"
+              justify="flex-start"
+              alignItems="flex-start">
+              <Grid item>
+                <Button type="submit" variant="contained" color="primary" disabled={!isValid}>
+                  Save
+                      </Button>
+              </Grid>
 
-                    <Grid item>
-                      <Button variant = "outlined">
-                      Cancel
+              <Grid item>
+                <Button variant="outlined">
+                  Cancel
                     </Button>
-                    </Grid>
-                  </Grid>
+              </Grid>
+            </Grid>
 
-                </div>
-               
-               )} />
+          </div>
+
+        )} />
     </form>
   );
 
